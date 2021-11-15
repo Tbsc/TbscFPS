c9c9c4e1d1974dd583a8bbee0098d69ae666161f
Tbsc <Tbsc@users.noreply.github.com>
2021-11-15 14:16:25 +0200

    Update to 1.16, with better positioning on screen and keybinds replacing the settings GUI

---
+++ src/main/java/tbsc/fps/CounterPosition.java
@@ -0,0 +1,12 @@
+package tbsc.fps;
+
+public enum CounterPosition {
+
+    TOP_LEFT,     TOP_MIDDLE,    TOP_RIGHT,
+                                CENTER_RIGHT,
+                                BOTTOM_RIGHT,
+                 BOTTOM_MIDDLE,
+    BOTTOM_LEFT,
+    CENTER_LEFT;
+
+}
